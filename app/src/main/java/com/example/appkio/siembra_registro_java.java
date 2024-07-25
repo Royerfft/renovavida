d31c92eae8709ba6c6b1133a9e6c4f78c913400f
royer <royerferney7@gmail.com>
2024-07-25 03:26:30 -0500

    ...

--- app/src/main/java/com/example/appkio/siembra_registro_java.java
+++ app/src/main/java/com/example/appkio/siembra_registro_java.java
@@ -6,14 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 
 public class siembra_registro_java extends AppCompatActivity {
@@ -29,7 +24,7 @@ public class siembra_registro_java extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pantallaregistro_siembrayterreno);
+        setContentView(R.layout.pantalla_regconsulta);
 
 
 
